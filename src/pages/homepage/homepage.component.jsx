8ae91efdeb18baa475af8f9aa83d09e292c750f4
Luiz Gustavo Matos de Lima <34725512+woldennore@users.noreply.github.com>
2022-01-26 21:01:02 -0300

    react-router-dom problem fix

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import './homepage.styles.scss';
+
 import Directory from '../../components/directory/directory.component';
 
+import './homepage.styles.scss';
+
 const HomePage = () => (
-  <Directory />
-)
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
 
 export default HomePage;
